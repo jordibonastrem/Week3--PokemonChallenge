ac9a166f0681f88d7bb1776df54ea498bc2e4f0d
Jordi Bonastre <jordi.bonastre.m@gmail.com>
2021-10-16 13:11:45 +0200

    Test connection to pokeApi

--- public/js/PokeApiConnection.js
+++ public/js/PokeApiConnection.js
@@ -1,1 +1,12 @@
-class PokeApiConnection {}
+class PokeApiConnection {
+  urlApi = "https://pokeapi.co/api/v2/pokemon";
+
+  async getPokemonPage() {
+    const response = await fetch(this.urlApi);
+    const pokemons = await response.json();
+    return pokemons;
+  }
+}
+
+p = new PokeApiConnection();
+p.getPokemonPage();
