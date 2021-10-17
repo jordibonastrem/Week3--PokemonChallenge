9714955a5fd0d2b8ce948a49e3c8b65841d5cf64
Jordi Bonastre <jordi.bonastre.m@gmail.com>
2021-10-17 18:39:20 +0200

    bugfix button pokedex

--- public/js/index.js
+++ public/js/index.js
@@ -10,7 +10,7 @@ const pokeApiConnection = new PokeApiConnection();
 let pageNumber = "0";
 const pokemonsAdedList = [];
 
-async function newPokemonArray(prevornext = "next") {
+async function newPokemonArray(prevornext = "next", pokedex = false) {
   if (pageNumber != 0) {
     document.querySelector(".main").remove();
   }
@@ -91,83 +91,7 @@ async function newPokemonArray(prevornext = "next") {
 }
 
 async function Pokedex() {
-  if (pageNumber != 0) {
-    document.querySelector(".main").remove();
-  }
-  if (prevornext === "next") {
-    pageNumber++;
-  } else {
-    pageNumber--;
-  }
-  if (pageNumber < 0) {
-    return;
-  }
-
-  const pokemonArr = [];
-  const pokemons = await pokeApiConnection.getPokemons(`${pageNumber * 10}`);
-  const results = await pokemons.results;
-
-  await Promise.all(
-    results.map(async (result) => {
-      const pokemonInfo = await pokeApiConnection.getPokemonInfo(result.url);
-
-      const nextPokemon = new Pokemon(
-        pokemonInfo.id,
-        pokemonInfo.name,
-        pokemonInfo.sprites.other.dream_world.front_default,
-        pokemonInfo.types,
-        pokemonInfo.stats[0].base_stat,
-        pokemonInfo.stats[1].base_stat,
-        pokemonInfo.stats[2].base_stat,
-        pokemonInfo.stats[5].base_stat,
-        pokemonInfo.types[0].type.name
-      );
-
-      pokemonArr.push(nextPokemon);
-    })
-  );
-  const page = new Page(document.querySelector("body"));
-  pokemonArr.forEach((pokemon) => {
-    const card = new PokemonCard(
-      document.querySelector(".cards__list"),
-      pokemon
-    );
-    const buttonAdd = card.element.querySelector(".button__add");
-    buttonAdd.addEventListener("click", () => {
-      pokemonsAdedList.push(pokemon);
-      console.log(pokemonsAdedList);
-    });
-  });
-
-  const element = document
-    .querySelector(".active")
-    .addEventListener("click", () => {
-      console.log("ERERE");
-    });
-
-  new PaginationComponent(
-    page.element.querySelector(".pagination"),
-    "pagination__previous",
-    "<",
-    newPokemonArray
-  );
-  new PaginationComponent(
-    page.element.querySelector(".pagination"),
-    "pagination__next",
-    ">",
-    newPokemonArray
-  );
-  new PokedexConnection(pokemonsAdedList);
-
-  const buttonNext = document.querySelector(".pagination__next");
-  const buttonPrev = document.querySelector(".pagination__previous");
-
-  buttonNext.addEventListener("click", () => {
-    newPokemonArray("next");
-  });
-  buttonPrev.addEventListener("click", () => {
-    newPokemonArray("prev");
-  });
+  newPokemonArray;
 }
 
 (async () => {
